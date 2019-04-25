871ac15f71fc111d16a66bfc2c9ff624e7ddf0c8
@lebarquero <lebarquero@gmail.com>
2019-04-24 23:31:39 -0600

    aspnetcoreapp & razorpagesmovie

---
+++ aspnetcoreapp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        // TODO :: Test Convenience methods ConfigureServices, Configure
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            // 1. Call CreateWebHostBuilder without params
+            // WebHost.CreateDefaultBuilder()
+            WebHost.CreateDefaultBuilder(args)
+                // 2. Change Startup class name
+                // .UseStartup<Startup2>();
+                .UseStartup<Startup>();
+    }
+}
